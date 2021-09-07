3c31c9c6217e801b725eff38027b0d317312a654
chall298 <chall361@gmail.com>
2021-09-06 22:07:50 -0400

    Made login and signup view

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import  { createTheme, ThemeProvider }  from '@material-ui/core/styles';
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
+
 // Components
 import Navbar from './components/Navbar'
+import AuthRoute from './util/AuthRoute'
 // Pages
 import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff',
-    }
-  },
-  typography: {
-    useNextVariants: true
+const theme = createTheme(themeFile)
+
+let authenticated;
+const token = localStorage.FBIdToken
+if(token){
+  const decodedToken = jwtDecode(token)
+  console.log(decodedToken)
+  if(decodedToken.exp * 1000 < Date.now()){
+    window.location.href = '/login'
+    authenticated = false
+  } else {
+    authenticated = true
   }
-})
+}
 
 function App() {
   return (
@@ -38,8 +37,8 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path="/" component={home}/>
-            <Route exact path="/login" component={login}/>
-            <Route exact path="/signup" component={signup}/>
+            <AuthRoute exact path="/login" component={login}/>
+            <AuthRoute exact path="/signup" component={signup}/>
           </Switch>
         </div>
       </Router>
---
+++ src/util/AuthRoute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthRoute = ({component: Component, authenticated, ...rest }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AuthRoute
