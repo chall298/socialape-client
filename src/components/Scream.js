59ebb930f64724714468902d259c4db8cdafcb31
chall298 <chall361@gmail.com>
2021-09-15 23:16:55 -0400

    turned the like button into a component

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
 import DeleteScream from './DeleteScream';
 import ScreamDialog from './ScreamDialog'
+import LikeButton from './LikeButton';
 // MUI stuff
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,8 +15,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 // Icons
 import ChatIcon from '@material-ui/icons/Chat'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
+
 // Redux
 import { connect } from 'react-redux'
 import { likeScream, unlikeScream } from '../redux/actions/dataActions';
@@ -36,19 +36,19 @@ const styles = {
 }
 
 class Scream extends Component {
-    likedScream = () => {
-        if(this.props.user.likes && this.props.user.likes.find(like => like.screamId === this.props.scream.screamId)){
-            return true
-        } else {
-            return false
-        }
-    }
-    likeScream = () => {
-        this.props.likeScream(this.props.scream.screamId)
-    }
-    unlikeScream = () => {
-        this.props.unlikeScream(this.props.scream.screamId)
-    }
+    // likedScream = () => {
+    //     if(this.props.user.likes && this.props.user.likes.find(like => like.screamId === this.props.scream.screamId)){
+    //         return true
+    //     } else {
+    //         return false
+    //     }
+    // }
+    // likeScream = () => {
+    //     this.props.likeScream(this.props.scream.screamId)
+    // }
+    // unlikeScream = () => {
+    //     this.props.unlikeScream(this.props.scream.screamId)
+    // }
     render() {
         dayjs.extend(relativeTime)
         const { 
@@ -67,23 +67,23 @@ class Scream extends Component {
                     credentials: { handle }
                 }
             } = this.props
-        const likeButton = !authenticated ? (
-            <MyButton tip ="Like">
-                <Link to="/login">
-                    <FavoriteBorder color="primary" />
-                </Link>
-            </MyButton>
-        ) : (
-            this.likedScream() ? (
-                <MyButton tip="Undo like" onClick={this.unlikeScream}>
-                    <FavoriteIcon color="primary"/>
-                </MyButton>
-            ) : (
-                <MyButton tip="Like" onClick={this.likeScream}>
-                    <FavoriteBorder color="primary"/>
-                </MyButton>
-            )
-        )
+        // const likeButton = !authenticated ? (
+        //     <MyButton tip ="Like">
+        //         <Link to="/login">
+        //             <FavoriteBorder color="primary" />
+        //         </Link>
+        //     </MyButton>
+        // ) : (
+        //     this.likedScream() ? (
+        //         <MyButton tip="Undo like" onClick={this.unlikeScream}>
+        //             <FavoriteIcon color="primary"/>
+        //         </MyButton>
+        //     ) : (
+        //         <MyButton tip="Like" onClick={this.likeScream}>
+        //             <FavoriteBorder color="primary"/>
+        //         </MyButton>
+        //     )
+        // )
         const deleteButton = authenticated && userHandle === handle ? (
             <DeleteScream screamId={screamId}/>
         ) : null
@@ -106,7 +106,8 @@ class Scream extends Component {
                     {deleteButton }
                     <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
-                    {likeButton}
+                    <LikeButton screamId={screamId}/>
+                    {/* {likeButton} */}
                     <span>{likeCount} Likes</span>
                     <MyButton tip="comments">
                         <ChatIcon color="primary"/>
@@ -123,8 +124,8 @@ class Scream extends Component {
 }
 
 Scream.propTypes = {
-    likeScream: PropTypes.func.isRequired,
-    unlikeScream: PropTypes.func.isRequired,
+    // likeScream: PropTypes.func.isRequired,
+    // unlikeScream: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     scream: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
@@ -134,9 +135,9 @@ const mapStateToProps = state => ({
     user: state.user
 })
 
-const mapActionsToProps = {
-    likeScream,
-    unlikeScream
-}
+// const mapActionsToProps = {
+//     likeScream,
+//     unlikeScream
+// }
 
-export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Scream))
+export default connect(mapStateToProps)(withStyles(styles)(Scream))
--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import MyButton from '../util/MyButton';
 import LikeButton from './LikeButton';
-import Comments from './Comments';
-import CommentForm from './CommentForm';
+// import Comments from './Comments';
+// import CommentForm from './CommentForm';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
 // MUI Stuff
@@ -53,9 +53,72 @@ class ScreamDialog extends Component {
     }
     handleOpen = () => {
         this.setState({ open: true })
+        this.props.getScream(this.props.screamId)
     }
     handleClose = () => {
         this.setState({ open: false })
+    }
+    render(){
+        const { classes, scream: { screamId, body, createdAt, likeCount, commentCount, userImage, userHandle }, UI: { loading } } = this.props
+        const dialogMarkup = loading ? (
+            <div className={classes.spinnerDiv}>
+                <CircularProgress size={200} thickness={2}/>
+            </div>
+        ) : (
+            <Grid container spacing={16}>
+                <Grid item sm={5}>
+                    <img src={userImage} alt="Profile" className={classes.profileImage}/>
+                </Grid>
+                <Grid item sm={7}>
+                    <Typography
+                        component={Link}
+                        color="primary"
+                        variant="h5"
+                        to={`/users/${userHandle}`}
+                    >
+                        @{userHandle}
+                    </Typography>
+                    <hr className={classes.invisibleSeparator}/> 
+                    <Typography variant="body2" color="textSecondary">
+                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                    </Typography>
+                    <hr className={classes.invisibleSeparator}/>
+                    <Typography variant="body1">
+                        {body}
+                    </Typography>
+                    <LikeButton screamId={screamId}/>
+                    <span>{likeCount} likes </span>
+                    <MyButton tip="comments">
+                        <ChatIcon color="primary"/>
+                    </MyButton>
+                    <span>{commentCount} comments</span>
+                </Grid>
+            </Grid>
+        )
+        return (
+            <Fragment>
+                <MyButton onClick={this.handleOpen} tip="Expand scream" tipClassName={classes.expandButton}>
+                    <UnfoldMore color="primary" />
+                </MyButton>
+                <Dialog
+              open={this.state.open}
+              onClose={this.handleClose}
+              fullWidth
+              maxWidth="sm"
+            >
+              <MyButton 
+                tip="Close" 
+                onClick={this.handleClose} 
+                tipClassName={classes.closeButton}
+              >
+                <CloseIcon />
+                </MyButton>
+                <DialogContent className={classes.dialogContent}>
+                    {dialogMarkup}
+                </DialogContent>
+                </Dialog>
+            </Fragment>
+        )
     }
 }
 
